1008a7fe96da5a77507ee502f8d80d5b5397f977
unknown <mosharafctg111@gmail.com>
2022-09-04 01:35:44 +0600

    card created

--- js/news.js
+++ js/news.js
@@ -27,7 +27,46 @@
         const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
         fetch(url)
         .then(Response => Response.json())
-        .then(data => console.log(data.data))
+        .then(data => showCard(data.data))
+    }
+
+    const fillCard = document.getElementById('category-Posts')
+    const showCard = (cards)=>{
+        for(const card of cards){
+            fillCard.innerHTML = '';
+            console.log(card);
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `
+            <div class="card mb-3">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div><div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+            `
+            fillCard.append(newDiv)
+        }
     }
 
 loadNewsdata()
