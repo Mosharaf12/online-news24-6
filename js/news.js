a25bfdfdcbb617921d1365caacfcdd154872169d
unknown <mosharafctg111@gmail.com>
2022-09-03 21:20:58 +0600

    added fetch link

---
+++ js/news.js
@@ -0,0 +1,26 @@
+const loadNewsdata =()=>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => displaydata(data.data.news_category))
+}
+const displaydata = catagories => {
+    // console.log(catagories);
+    const ulCatagory = document.getElementById('ul-catagory');
+
+    for (const category of catagories) {
+        const allCatagories = category.category_name;
+        // console.log(allCatagories);
+        const li = document.createElement('li');
+        li.classList.add('me-3')
+        li.innerHTML = `
+            <a onclick="alllink('${category.category_id}')" class="btn categoryBtn">${allCatagories}</a>
+        `
+        ulCatagory.appendChild(li);
+        // isLoading(true);
+    }
+
+}
+
+
+loadNewsdata()
