bacba2f04710b4f0c554b639c30528d0b7a2a1c2
unknown <mosharafctg111@gmail.com>
2022-09-04 01:18:07 +0600

    click function added

--- js/news.js
+++ js/news.js
@@ -1,26 +1,33 @@
-const loadNewsdata =()=>{
-    const url = `https://openapi.programming-hero.com/api/news/categories`
-    fetch(url)
-    .then(Response => Response.json())
-    .then(data => displaydata(data.data.news_category))
-}
-const displaydata = catagories => {
-    // console.log(catagories);
-    const ulCatagory = document.getElementById('ul-catagory');
+    const loadNewsdata =()=>{
+        const url = `https://openapi.programming-hero.com/api/news/categories`
+        fetch(url)
+        .then(Response => Response.json())
+        .then(data => displaydata(data.data.news_category))
+    }
+    const displaydata = catagories => {
+        // console.log(catagories);
+        const ulCatagory = document.getElementById('ul-catagory');
 
-    for (const category of catagories) {
-        const allCatagories = category.category_name;
-        // console.log(allCatagories);
-        const li = document.createElement('li');
-        li.classList.add('me-3')
-        li.innerHTML = `
-            <a onclick="alllink('${category.category_id}')" class="btn categoryBtn">${allCatagories}</a>
-        `
-        ulCatagory.appendChild(li);
-        // isLoading(true);
+        for (const category of catagories) {
+            const allCatagories = category.category_name;
+            // console.log(allCatagories);
+            const li = document.createElement('li');
+            li.classList.add('me-3')
+            li.innerHTML = `
+                <a onclick="alllink('${category.category_id}')" class="btn categoryBtn">${allCatagories}</a>
+            `
+            ulCatagory.appendChild(li);
+            // isLoading(true);
+        }
+
     }
 
-}
-
+    const alllink = (id)=> {
+        console.log('button cliked');
+        const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+        fetch(url)
+        .then(Response => Response.json())
+        .then(data => console.log(data.data))
+    }
 
 loadNewsdata()
